9ef5ea8cfbd20b7e3ca105c8a2b9a821a20a19f7
leitongchen <chen@aprileconsulting.com>
2024-03-06 16:56:21 +0100

    Add more fields to Book form; start to add styling for a dropdown

--- book.js
+++ book.js
@@ -1,11 +1,18 @@
 class Book {
-  constructor(_title, _authors, _publicationYear, _publisher, _isbn, _price, _languages) {
+  constructor(
+    _title,
+    _authors,
+    _publicationYear,
+    _publisher,
+    _isbn,
+    _price,
+    _languages
+  ) {
     this.title = _title;
     this.authors = _authors;
     this.publicationYear = _publicationYear;
     this.publisher = _publisher;
     this.isbn = _isbn;
     this.price = _price;
-    this.languages = _languages;
   }
 }
